52475c57639d3ff53f1b97ff3592b5de9f1f4d2e
HX-Wrdzgzs <141598320+HX-Wrdzgzs@users.noreply.github.com>
2026-07-29 10:36:53 +0800

    feat: add status badge component

---
+++ docs/.vitepress/theme/components/StatusBadge.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="amia-status-badge" :data-status="status">
+    <slot />
+  </span>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  status?: 'stable' | 'online' | 'beta' | 'info' | 'experimental' | 'maintenance' | 'deprecated' | 'offline'
+}>(), {
+  status: 'info'
+})
+</script>
